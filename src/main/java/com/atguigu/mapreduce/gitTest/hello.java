abcdc3337f0df0da592f9cbb22a27a39a82f016c
msx <m894572503@gmail.com>
2021-04-28 15:53:39 +0800

    second try

--- src/main/java/com/atguigu/mapreduce/gitTest/hello.java
+++ src/main/java/com/atguigu/mapreduce/gitTest/hello.java
@@ -3,5 +3,6 @@ package com.atguigu.mapreduce.gitTest;
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
